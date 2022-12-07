97ab0e157d18c8185dc461d37127ec90dba1352a
zombieFireITA <61471823+zombieFireITA@users.noreply.github.com>
2022-12-08 00:04:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const server = http.createServer((req, res) => {
     req.headers['user-agent'] &&
     !req.headers['user-agent'].includes('curl')
   ) {
-    res.writeHead(302, { Location: 'https://github.com/hugomd/parrot.live' });
+    res.writeHead(302, { Location: 'https://github.com/zombieFireITA/roccia' });
     return res.end();
   }
 
